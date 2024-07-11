7eb68d1760cf2db4d800ef13072a7d5728eb43e6
JagadeshAnbu <tajagadesh@gmail.com>
2024-07-11 16:09:34 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header=({handleToggleDarkMode})=>{
+    return(
+        <div className="header">
+            <h1>Notes</h1>
+            <button onClick={()=>handleToggleDarkMode(
+                (previouseDarkMode) => !previouseDarkMode
+            )} className="save">Toggle Mode</button>
+        </div>
+    )
+}
+export default Header;
